0bb2a6a4fd631713b5184ba87f4494c0dee958f9
Yucheng Zhang <ustczyc@outlook.com>
2019-10-07 15:45:42 -0400

    first working

--- gsm.py
+++ gsm.py
@@ -4,6 +4,7 @@ from scipy import integrate, interpolate
 import collections
 import sys
 from . import glq
+import time
 
 
 def peak_background_split(nu):
@@ -41,9 +42,12 @@ class gsm:
         self.data['r'] = None
         self.sigma_shift = None
 
-        # mu samples and weights
+        # s, mu samples and weights
+        self.s_arr = None
         self.mu_arr = None
         self.mu_w = None
+        self.r_sigma_arr = None
+        self.r_pi_arr = None
 
         # real-space interpolated function
         self.xi = None
@@ -52,6 +56,11 @@ class gsm:
         self.v_L = None
         self.sigma_p = None
         self.sigma_v = None
+
+        # y samples
+        self.ys = None
+        self.y_spanning = None
+        self.dy = None
 
     def read_clpt(self, fn_xi='', fn_v='', fn_s=''):
         '''Read CLPT output.'''
@@ -67,17 +76,27 @@ class gsm:
         # self.xi_L = interpolate.interp1d(self.data['r'], self.clpt['xi'][:, 1],
         #                                  kind='linear', bounds_error=True)
 
-    def set_mu_sample(self, kind=None):
-        '''Set mu samples and weights.'''
-        if kind == 'absave':
+    def set_s_mu_sample(self, s_arr, mu_ave=None):
+        '''Set s & mu samples and weights.'''
+        self.s_arr = s_arr
+        if mu_ave == 'absave':
             self.mu_arr = np.array([0.01*i-0.005 for i in range(1, 101, 1)])
             self.mu_w = np.full(100, 0.01) * 2
-        elif kind == 'ave':
+        elif mu_ave == 'ave':
             self.mu_arr = np.array([0.01*i-0.005 for i in range(-99, 101, 1)])
             self.mu_w = np.full(200, 0.01)
         else:
             self.mu_arr = glq.gl_x
             self.mu_w = glq.gl_w
+
+        # transverse and los projection of s
+        self.r_sigma_arr = np.zeros((self.s_arr.shape[0],
+                                     self.mu_arr.shape[0]))
+        self.r_pi_arr = np.zeros((self.s_arr.shape[0], self.mu_arr.shape[0]))
+        for i, s in enumerate(self.s_arr):
+            for j, mu in enumerate(self.mu_arr):
+                self.r_sigma_arr[i, j] = s * np.sqrt(1. - mu**2)  # transverse
+                self.r_pi_arr[i, j] = s * mu  # los
 
     def set_pars(self, nu=None, f_v=None, sFOG=None, sigma_p_100=27):
         '''Set RSD parameters.'''
@@ -90,8 +109,6 @@ class gsm:
         fb11b21 = F1 * F2
         fb12b20 = F1 * F1
         fb10b22 = F2 * F2
-
-        print(fb11b20, fb10b21, fb11b21, fb12b20, fb10b22)
 
         self.pars['nu'] = nu
         self.pars['f_v'] = f_v
@@ -140,60 +157,76 @@ class gsm:
 
         self.sigma_shift = self.sigma_p(100.) - sigma_p_100
 
-    def _sigma2(self, r, mu):
+    def _sigma2(self, r, mu_r):
         '''sigma_12^2(r,mu).'''
-        mu2 = mu**2
-        res = mu2 * self.sigma_p(r)
-        res += self.sigma_v(r) * (1. - mu2) * 0.5
+        mu_r2 = mu_r**2
+        res = mu_r2 * self.sigma_p(r)
+        res += self.sigma_v(r) * (1. - mu_r2) * 0.5
         res -= self.sigma_shift
         res += self.pars['sFOG']**2
         return res
 
-    def _kernel(self, y, mu, r_pi, r_sigma):
+    def _kernel(self, y, r_pi, r_sigma):
         '''integrand'''
         r = np.sqrt(r_sigma**2 + y**2)  # real-space separation distance
+        mu_r = y / r
         if r < 3.:
             return 0.
-        sigma2 = self._sigma2(r, mu)
+        sigma2 = self._sigma2(r, mu_r)
         if sigma2 < 0.:
             return 0.
-        exp_idx = -0.5 * (r_pi - y - mu * self.v(r))**2 / sigma2
+        exp_idx = -0.5 * (r_pi - y - mu_r * self.v(r))**2 / sigma2
 
         return (1. + self.xi(r)) * np.exp(exp_idx) / np.sqrt(2 * np.pi * sigma2)
 
-    def _kernel_arr(self, y, mu, r_pi, r_sigma):
+    def _kernel_arr(self, y, r_pi, r_sigma):
         '''integrand for numpy array of y'''
         r = np.sqrt(r_sigma**2 + y**2)  # real-space separation distance
-        sigma2 = self._sigma2(r, mu)
+        mu_r = y / r
+        sigma2 = self._sigma2(r, mu_r)
         res = np.zeros_like(y)
         idx = (r < 3.) | (sigma2 < 0.)
 
-        exp_idx = -0.5 * (r_pi - y - mu * self.v(r))**2 / sigma2
+        exp_idx = -0.5 * (r_pi - y - mu_r * self.v(r))**2 / sigma2
         res = (1. + self.xi(r)) * np.exp(exp_idx) / \
             np.sqrt(2 * np.pi * sigma2)
         res[idx] = 0.
 
         return res
 
-    def c_xi(self, s_arr, y_spanning=200, dy=0.5, int_m=None):
+    def set_y_sample(self, y_spanning=200, dy=0.5):
+        '''Set y samples.'''
+        self.y_spanning = y_spanning
+        self.dy = dy
+        self.ys = collections.OrderedDict()
+        for i in range(self.s_arr.shape[0]):
+            self.ys[i] = collections.OrderedDict()
+            for j in range(self.mu_arr.shape[0]):
+                r_pi = self.r_pi_arr[i, j]
+                self.ys[i][j] = np.arange(r_pi-y_spanning, r_pi+y_spanning, dy)
+
+    def c_xi(self, int_m=None):
         '''Compute RSD xi_0 and xi_2.'''
-        xi2d = np.zeros((s_arr.shape[0], self.mu_arr.shape[0]))
+        t0 = time.time()
+        xi2d = np.zeros((self.s_arr.shape[0], self.mu_arr.shape[0]))
 
         if int_m == 'scipy':
-            for i, s in enumerate(s_arr):
-                for j, mu in enumerate(self.mu_arr):
-                    r_sigma = s * np.sqrt(1. - mu**2)  # transverse
-                    r_pi = s * mu  # los
-                    xi2d[i, j] = integrate.quad(self._kernel, r_pi-y_spanning, r_pi+y_spanning,
-                                                args=(mu, r_pi, r_sigma,), epsabs=0., epsrel=1e-2)[0] - 1
+            for i in range(self.s_arr.shape[0]):
+                for j in range(self.mu_arr.shape[0]):
+                    r_sigma = self.r_sigma_arr[i, j]
+                    r_pi = self.r_pi_arr[i, j]
+                    xi2d[i, j] = integrate.quad(self._kernel,
+                                                r_pi-self.y_spanning, r_pi+self.y_spanning,
+                                                args=(r_pi, r_sigma,),
+                                                epsabs=0., epsrel=1e-2)[0] - 1
         else:
-            for i, s in enumerate(s_arr):
-                for j, mu in enumerate(self.mu_arr):
-                    r_sigma = s * np.sqrt(1. - mu**2)  # transverse
-                    r_pi = s * mu  # los
-                    ys = np.arange(r_pi-y_spanning, r_pi+y_spanning, dy)
-                    res = self._kernel_arr(ys, mu, r_pi, r_sigma)
-                    xi2d[i, j] = np.sum(0.5 * dy * (res[1:]+res[:-1])) - 1
+            for i in range(self.s_arr.shape[0]):
+                for j in range(self.mu_arr.shape[0]):
+                    r_sigma = self.r_sigma_arr[i, j]
+                    r_pi = self.r_pi_arr[i, j]
+                    ys = self.ys[i][j]
+                    res = self._kernel_arr(ys, r_pi, r_sigma)
+                    xi2d[i, j] = np.sum(0.5 * self.dy * (res[1:]+res[:-1])) - 1
 
         xi2d_w = xi2d * self.mu_w
         # monopole
@@ -205,4 +238,5 @@ class gsm:
         fac = 5. / 2.
         xi_2 = fac * np.sum(xi2d_w * L_mu, axis=1)
 
+        print('>> time elapsed: {0:.2f} s'.format(time.time()-t0))
         return xi_0, xi_2
